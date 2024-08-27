2afead943e84ea30a43b0dec84c92da9bf0f1412
MAHossain1 <mahpro110@gmail.com>
2024-08-27 18:23:17 +0600

    Implemented NotFound page.

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import MaximumWidthWrapper from '../components/shared/MaximumWidthWrapper';
+import { Link } from 'react-router-dom';
+import { useAppSelector } from '../redux/hooks';
+import { selectUser } from '../redux/features/auth/authSlice';
 
 const NotFound = () => {
+  const user = useAppSelector(selectUser);
+
   return (
-    <div>
-      <h1>Not found</h1>
-    </div>
+    <MaximumWidthWrapper className="grid place-content-center gap-3 mt-20">
+      <ul className="text-blue-500 text-xl">
+        <li>
+          <Link to="/">Go to Home</Link>
+        </li>
+
+        {user ? (
+          <li>
+            <Link to="/services">Services</Link>
+          </li>
+        ) : (
+          <li>
+            <Link to="/login">Go to Login</Link>
+          </li>
+        )}
+      </ul>
+
+      <img className="h-screen w-screen" src="/public/error.png" alt="" />
+    </MaximumWidthWrapper>
   );
 };
 
